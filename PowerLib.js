6132401fa6735d800fc917fd6f5bb6e118ab4940
rnpowerconsulting <rnpowerconsulting@users.noreply.github.com>
2014-02-20 22:45:25 -0500

    Added refined structuring and support for modules.

--- PowerLib.js
+++ PowerLib.js
@@ -1,211 +1,234 @@
-var PowerLib = {
-    support : {
-        input : {
-            ___placeholder : null,
-            placeholder : function () {
-                if (PowerLib.support.input.___placeholder != null) {
-                    var i = document.createElement('input');
-                    PowerLib.support.input.placeholder('placeholder' in i);
-                    i = null;
-                }
+//accept instance of pl in order to provide proper interaction between the modules
+var PowerLibSupport = function (pl) {
+	//place a reference to this "context" tp self so that it can be referenced inside of child functions
+	var self = this;
 
-                return PowerLib.support.input.___placeholder;
-            }
-        }
-    },
+	//Check if input placeholder is supported
+	self.input = {
+		___placeholder: null,
+		placeholder: function () {
+			if (PowerLib.support.input.___placeholder != null) {
+				var i = document.createElement('input');
+				PowerLib.support.input.placeholder('placeholder' in i);
+				i = null;
+			}
 
-debug: true,
-    isDefined: function (obj) {
-        if (obj == null || typeof (obj) == 'undefined') {
-            return false;
-        }
-        //In case for some reason obj is set to the text of undefined, not just it's type. I have seen this on several occasions, when working with 3rd party code.
-        if (obj === 'undefined')
-            return false;
-
-        return true;
-    },
-    isArray: function (obj) {
-        if (!PowerLib.isDefined(obj))
-            return false;
-        return obj.constructor == Array;
-    },
-
-    isInArray: function (item, array) {
-        var length = array.length;
-        for (var i = 0; i < length; i++) {
-            if (array[i] == item) return true;
-        }
-        return false;
-    },
-    //instead of using console.log everywhere, I use PowerLib.log(), which will write to the console only of debug is set to true and console exists, prevents IE problems
-    //I also store a copy of the log here, in case the console was not available, we can still retrieve the log at a later point, this is extremely valuable for debugging, but be careful with production security.
-    logValues: [],
-    log: function (text) {
-        if (PowerLib.debug)
-            PowerLib.logValues.push(text);
-
-        try {
-            if (PowerLib.debug && PowerLib.isDefined(console)) {
-                console.log(text);
-            }
-        } catch (er1) { }
-
-    },
-
-    /*
-        Handlers functionality, add enums for various handle type support. 
-    */
-    handlers: {},
-    handleTypes: { error: 'error', info: 'info' },
-    addHandler: function (id, type, func) {
-        PowerLib.log('PowerLib.addHandler(id[' + id + '], type[' + type + '])');
-        PowerLib.handlers[id] = { id: id, type: type, func: func };
-    },
-
-    removeHandler: function (id) {
-        PowerLib.log('PowerLib.removeHandler(id[' + id + '])');
-        PowerLib.handlers[id] = null;
-        delete PowerLib.handlers[id];
-    },
-
-    fireHandler: function (type, result, data) {
-        PowerLib.log('PowerLib.fireHandler(type[' + type + '], result[' + result + '], data[' + data + '])');
-        for (var index in PowerLib.handlers) {
-            var handle = PowerLib.handlers[index];
-            if (handle.type == type) {
-                handle.func.apply(null, [result, data]);
-            }
-        }
-    },
-    get: function (id) {
-        //Yes, I actually have this... For when I don't have access to JQuery ;-)
-        return document.getElementById(id);
-    },
-    _queryData : null,
-    getQueryValue : function(key){
-        //If exists, we will use existing data, no need to reparse the data. querystring should never changes once you are on the page.
-        if (!PowerLib.isDefined(_queryData)){
-            //Get keysets, remove the ? and split into pairs
-            var keySets = location.search.slice(1).split('&');
+			return PowerLib.support.input.___placeholder;
+		}
+	};
 
 
-            keySets.forEach(function(keySet) {
-                var keyValue = keySet.split('=');
-                PowerLib._queryData[keyValue[0]] = keyValue[1]
-            });
-        }
-
-        return _queryData[key];
-    },
-    //I found this function somewhere and have used it a lot in the past, it is here especially for legacy code. If anyone knows its source, please let me know so I can credit properly.
-    getAttributes: function (ele, attr) {
-        var result = (ele.getAttribute && ele.getAttribute(attr)) || null;
-        if (!result) {
-            var attrs = ele.attributes;
-            var length = attrs.length;
-            for (var i = 0; i < length; i++)
-                if (attrs[i].nodeName === attr)
-                    result = attrs[i].nodeValue;
-        }
-        return result;
-    },
-    //I use this a lot for getting epoc dates, to deal with .net dates. It is great for storing 
-    getEpoch: function () {
-        return Math.round(new Date().getTime() / 1000.0);
-    },
-    onInputKeyDownSubmit: function (e, elm, func) {
-        var evt = e || window.event
-        if (evt.keyCode === 13) {
-            func.apply();
-        }
-    },
-    /*These two functions are for lagacy browsers, that don't have placeholder support yet*/
-    onInputBlur: function (elm) {
-        if (PowerLib.support.input.placeholder())
-            return;
-        if (elm.value == '') {
-            var titleAttribute = PowerLib.getAttributes(elm, 'placeholder');
-            elm.value = titleAttribute;
-        }
-    },
-    onInputFocus: function (elm) {
-        if (PowerLib.support.input.placeholder())
-            return;
-        var titleAttribute = PowerLib.getAttributes(elm, 'placeholder');
-        if (elm.value == titleAttribute) {
-            elm.value = '';
-        }
-    },
-    //I am not sure where I got this from, if you know, please feel free to let me know.
-    getElementBoundaries : function (elm) {
-        var bcrect, od, odb, odde, dimensions = { "left": 0, "top": 0, "right": 0, "bottom": 0, "width": 0, "height": 0 };
-
-        if (typeof (elm) != 'undefined') {
-            od = elm.ownerDocument;
-            odb = od.body;
-            odde = od.documentElement;
-            bcrect = elm.getBoundingClientRect();
-            dimensions.left = bcrect.left;
-            dimensions.top = bcrect.top;
-            dimensions.right = bcrect.right;
-            dimensions.bottom = bcrect.bottom;
-
-            if (bcrect.width) {
-                dimensions.width = bcrect.width;
-                dimensions.height = bcrect.height;
-            }
-            else {
-                dimensions.width = dimensions.right - dimensions.left;
-                dimensions.height = dimensions.bottom - dimensions.top;
-            }
-
-            if (odb.scrollTop) {
-                dimensions.top += odb.scrollTop;
-                dimensions.left += odb.scrollLeft;
-            }
-            else if (odde && odde.scrollTop) {
-                dimensions.top += odde.scrollTop;
+	//Check if local storage exists and is available to use
+	self.localStorage = function () {
+		return 'localStorage' in window && window['localStorage'] !== null;
+	}
 
 
-                dimensions.left += odde.scrollLeft;
-            }
-        }
-        return dimensions;
-    },
-    newGuid: function () {
-        var s4 = function () {
-            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        };
-        return (s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4());
-    },
-    getStorage : function (key) {
-        PowerLib.log('PowerLib.getStorage(key[' + key + '])');
-        if (localStorage) {
-            var value = localStorage[key];
-            if (!PowerLib.isDefined(value))
-                return null;
-            try {
-                return JSON.parse(localStorage[key]);
-            } catch (ex) {
-                return localStorage[key];
-            }
-        }
-        return null;
-    },
-    setStorage: function (key, value) {
-        PowerLib.log('PowerLib.setStorage(key[' + key + '])');
-        if (typeof value != "string" && PowerLib.isDefined(value))
-            value = JSON.stringify(value);
-        try {
-            if (localStorage)
-                localStorage[key] = value;
-        } catch (storageEx) {
-            PowerLib.log('PowerLib.setStorage(key[' + key + ']) -> Error: ' + storageEx.message);
-        }
-    }
+	return self;
 };
+
+
+//accept instance of pl in order to provide proper interaction between the modules
+var PowerLibUtils = function (pl) {
+	//place a reference to this "context" tp self so that it can be referenced inside of child functions
+	var self = this;
+
+	//Returns epoch value that can be used for communicating with the server.
+	self.getEpoch = function () {
+		return Math.round(new Date().getTime() / 1000);
+	};
+
+	var _queryData = null;
+	self.getQueryValue = function (key) {
+		//If exists, we will use existing data, no need to reparse the data. querystring should never changes once you are on the page.
+		if (!pl.isDefined(_queryData)) {
+			//Get keysets, remove the ? and split into pairs
+			var keySets = location.search.slice(1).split('&');
+
+			keySets.forEach(function (keySet) {
+				var keyValue = keySet.split('=');
+				_queryData[keyValue[0]] = keyValue[1]
+			});
+		}
+
+		return _queryData[key];
+	};
+
+	//trap the enter submission, just a quick util to save the time of writing this time and time again.
+	self.onInputKeyDownSubmit = function (e, elm, func) {
+		var evt = e || window.event
+		if (evt.keyCode === 13) {
+			func.apply(this, [elm]);
+		}
+	};
+
+	/*These two functions are for legacy browsers, that don't have placeholder support yet*/
+	self.onInputBlur = function (elm) {
+		if (pl.support.input.placeholder())
+			return;
+		if (elm.value == '') {
+			var titleAttribute = pl.getAttributes(elm, 'placeholder');
+			elm.value = titleAttribute;
+		}
+	};
+
+	onInputFocus = function (elm) {
+		if (pl.support.input.placeholder())
+			return;
+		var titleAttribute = pl.getAttributes(elm, 'placeholder');
+		if (elm.value == titleAttribute) {
+			elm.value = '';
+		}
+	};
+
+	//I am not sure where I got this from, if you know, please feel free to let me know so I can give proper credits.
+	selfgetElementBoundaries = function (elm) {
+		var bcrect, od, odb, odde, dimensions = { "left": 0, "top": 0, "right": 0, "bottom": 0, "width": 0, "height": 0 };
+
+		if (typeof (elm) != 'undefined') {
+			od = elm.ownerDocument;
+			odb = od.body;
+			odde = od.documentElement;
+			bcrect = elm.getBoundingClientRect();
+			dimensions.left = bcrect.left;
+			dimensions.top = bcrect.top;
+			dimensions.right = bcrect.right;
+			dimensions.bottom = bcrect.bottom;
+
+			if (bcrect.width) {
+				dimensions.width = bcrect.width;
+				dimensions.height = bcrect.height;
+			}
+			else {
+				dimensions.width = dimensions.right - dimensions.left;
+				dimensions.height = dimensions.bottom - dimensions.top;
+			}
+
+			if (odb.scrollTop) {
+				dimensions.top += odb.scrollTop;
+				dimensions.left += odb.scrollLeft;
+			}
+			else if (odde && odde.scrollTop) {
+				dimensions.top += odde.scrollTop;
+
+
+				dimensions.left += odde.scrollLeft;
+			}
+		}
+		return dimensions;
+	};
+	
+	//Sinple version of generating guids, this is not timebased.
+	self.getGuid = function () {
+		var s4 = function () {
+			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+		};
+		return (s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4());
+	};
+
+
+	//Functions to set the storage, without generating errors if it fails. since this fails silently, first call pl.support.localStorage() to check if localStorage is supported.
+	self.getStorage = function (key) {
+		pl.log('PowerLib.getStorage(key[' + key + '])');
+		if (localStorage) {
+			var value = localStorage[key];
+			if (!pl.isDefined(value))
+				return null;
+			try {
+				return JSON.parse(localStorage[key]);
+			} catch (ex) {
+				return localStorage[key];
+			}
+		}
+		return null;
+	};
+
+	self.setStorage = function (key, value) {
+		pl.log('PowerLib.setStorage(key[' + key + '])');
+		if (typeof value != "string" && pl.isDefined(value))
+			value = JSON.stringify(value);
+		try {
+			if (localStorage)
+				localStorage[key] = value;
+		} catch (storageEx) {
+			pl.log('PowerLib.setStorage(key[' + key + ']) -> Error: ' + storageEx.message);
+		}
+	};
+
+	return self;
+};
+
+var PowerLib = function (conf) {
+	//place a reference to this "context" to self, so it can be referenced inside of child functions
+	var self = this;
+
+	//Pass in any value and get an absolute true in case it is defined, this function takes in account some older browsers and how they handle null values
+	self.isDefined = function (value) {
+		if (value == null || typeof value == 'undefined')
+			return false;
+		//In case for some reason obj is set to the text of undefined, not just it's type. I have seen this on several occasions, when working with 3rd party code.
+		if (obj === 'undefined')
+			return false;
+
+		return true;
+	};
+
+	//
+	self.isArray = function (obj) {
+		if (!self.isDefined(obj))
+			return false;
+		return obj.constructor == Array;
+	};
+
+	self.isInArray = function (item, array) {
+		var length = array.length;
+		for (var i = 0; i < length; i++) {
+			if (array[i] == item) return true;
+		}
+		return false;
+	};
+
+	//returns true if the passed in value is a true function
+	self.isFunction = function (functionRef) {
+		var checkableType = {};
+		return functionRef && checkableType.toString.call(functionRef) === '[object Function]';
+	};
+
+	//make sure that if no config is passed in, when we check for values we don't get undefined errors
+	if (!self.isDefined(conf))
+		conf = {};
+
+	self.debug = conf.debug || false;
+
+	//Logging function to log to the console that prevents some browsers (e.g IE) from throwing errors when dev tools are not enabled
+	//Passing in level 2 will trigger an error log, increasing visibility in the console.log when used sparingly
+	self.log = function (value, level) {
+		//if we are not in debug mode, we don't want to have the logging enabled.
+		if (!self.debug)
+			return;
+
+		if (self.isDefined(console) && self.isFunction(console.log)) {
+			if (level == 2)
+				console.error(value);
+			else
+				console.log(value);
+		}
+	};
+
+	//Create instance of the utilities module, bc self is an object when we pass it to the utils module, it will be passed as a reference.
+	self.util = new PowerLibUtils(self);
+	self.support = new PowerLibSupport(self);
+	
+	return self;
+};
+
+if (typeof pl != 'undefined') {
+	pl = new PowerLib({
+		debug : false
+	});
+}
+
 /*Prototypes*/
-if (!PowerLib.isDefined(String.prototype.contains)) {
-    String.prototype.contains = function (value) { return this.indexOf(value) != -1; };
+if (!pl.isDefined(String.prototype.contains)) {
+	String.prototype.contains = function (value) { return this.indexOf(value) != -1; };
 }
